8d3e48c4f2f0a2d270f8d60d9799a4c019facbb6
annabartholdy <136979809+annabartholdy@users.noreply.github.com>
2025-06-11 06:13:01 +0000

    Deploying to gh-pages from @ UCPH-SCARB/UCPH-SCARB.github.io@6d9bf7cf9a6c5868d3fb8e2c5adbb025b9e94d80 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -48,4 +48,9 @@ ninja.data = [{
           description: "Make your own project with supervisor Robin Andersson",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_projects/";
+            },},{id: "projects-supervisor-robert-krautz",
+          title: 'Supervisor Robert Krautz',
+          description: "Make your own project with supervisor Robert Krautz",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_projects/";
             },},];

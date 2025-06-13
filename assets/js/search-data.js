8dc64da802aa1bc797bf701ec7c0a9bf9210f356
annabartholdy <136979809+annabartholdy@users.noreply.github.com>
2025-06-13 11:07:34 +0000

    Deploying to gh-pages from @ UCPH-SCARB/UCPH-SCARB.github.io@919dc821a5c105bc5e4d3ce3fdf7ecb872d67578 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -40,7 +40,7 @@ ninja.data = [{
               window.location.href = "/projects/3_project/";
             },},{id: "projects-winther-group-supervision",
           title: 'Winther Group Supervision',
-          description: "Make your own project with Assistant Professor Robert Krautz.",
+          description: "Make your own project with Professor Ole Winther.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},];

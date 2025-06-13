e2558a65099ae054b5ff6b0d4daf1e20fdfc60f1
annabartholdy <136979809+annabartholdy@users.noreply.github.com>
2025-06-13 13:10:34 +0000

    Deploying to gh-pages from @ UCPH-SCARB/UCPH-SCARB.github.io@5a8a128dcaf2693813256df2dee3859dc082aa5e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -33,11 +33,6 @@ ninja.data = [{
           description: "Make your own project with Professor Ole Winther.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-supervisor-sarah-rennie",
-          title: 'Supervisor Sarah Rennie',
-          description: "Make your own project with Assistant Professor Sarah Rennie.",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-supervisor-robert-krautz",
           title: 'Supervisor Robert Krautz',
           description: "Make your own project with Assistant Professor Robert Krautz.",
@@ -48,4 +43,9 @@ ninja.data = [{
           description: "Make your own project with Associate Professor Robin Andersson.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Robin_project/";
+            },},{id: "projects-supervisor-sarah-rennie",
+          title: 'Supervisor Sarah Rennie',
+          description: "Make your own project with Assistant Professor Sarah Rennie.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Sarah_project/";
             },},];

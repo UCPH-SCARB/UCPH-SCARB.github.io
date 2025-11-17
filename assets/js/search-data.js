485370d01ad1635fe87219af0f6e2eecabcc60c3
annabartholdy <136979809+annabartholdy@users.noreply.github.com>
2025-11-17 09:22:40 +0000

    Deploying to gh-pages from @ UCPH-SCARB/UCPH-SCARB.github.io@3e6e0a7101ea6c674f3678b0a3d9b76ad571fde0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,6 +72,11 @@ ninja.data = [{
           description: "Make your own project with Professor Ole Winther.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Ole_project/";
+            },},{id: "projects-supervisor-peter-brodersen",
+          title: 'Supervisor Peter Brodersen',
+          description: "Make your own project with Professor Peter Brodersen.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Peter_project/";
             },},{id: "projects-supervisor-rasmus-heller",
           title: 'Supervisor Rasmus Heller',
           description: "Make your own project with Associate Professor Rasmus Heller.",
